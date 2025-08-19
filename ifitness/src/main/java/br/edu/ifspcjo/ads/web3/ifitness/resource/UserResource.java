e4d17a31bc7e5de965839a5fdeb82740f69b9a22
CJ3028607 <CJ3028607@192.168.56.1>
2025-08-18 22:27:04 -0300

    ifitness 18/08/25

--- ifitness/src/main/java/br/edu/ifspcjo/ads/web3/ifitness/resource/UserResource.java
+++ ifitness/src/main/java/br/edu/ifspcjo/ads/web3/ifitness/resource/UserResource.java
@@ -1,40 +1,42 @@
 package br.edu.ifspcjo.ads.web3.ifitness.resource;
 
-import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.edu.ifspcjo.ads.web3.ifitness.domain.model.Gender;
 import br.edu.ifspcjo.ads.web3.ifitness.domain.model.User;
-
+import br.edu.ifspcjo.ads.web3.ifitness.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
+@RequestMapping("/users")
 public class UserResource {
 	
-		@GetMapping("/users")
-		public List<User> list(){
-			User user1 = new User();
-			user1.setId(1L);
-			user1.setName("Juliana Santos");
-			user1.setEmail("julianasantos@email.com");
-			user1.setPassword("1234");
-			user1.setBirthDate(LocalDate.of(1995, 1, 1));
-			user1.setGender(Gender.FEMININO);
-			user1.setActive (true);
-			
-			User user2 = new User();
-			user2.setId(2L);
-			user2.setName("Adriana Silva");
-			user2.setEmail("adrianasilva@email.com");
-			user2.setPassword("5678");
-			user2.setBirthDate(LocalDate.of(2000, 8, 4));
-			user2.setGender(Gender.FEMININO);
-			user2.setActive (true);
-			
-			return Arrays.asList(user1, user2);
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	@PostMapping
+	public User create(@RequestBody User user, HttpServletResponse response) {
+		return userRepository.save(user);
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		Optional<User> user = userRepository.findById(id);
+		if(user.isPresent()) {
+			return ResponseEntity.ok(user.get());
 		}
-
-}
+		return ResponseEntity.notFound().build();
+	}
+	}
